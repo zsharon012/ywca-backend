cbf8cdbf378a5f2d6505d711c07513554eeadc43
slzeng89098 <slzeng@gmail.com>
2026-05-29 11:56:12 -0500

    Fix

--- src/server.js
+++ src/server.js
@@ -34,7 +34,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://ywca-disc.web.app');
   res.header('Access-Control-Allow-Credentials', 'true');
